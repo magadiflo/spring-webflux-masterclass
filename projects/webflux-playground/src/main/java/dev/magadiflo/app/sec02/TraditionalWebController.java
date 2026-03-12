8ba02448a90727426bee8479b42caa34f62191ce
Martín Díaz <magadiflo@gmail.com>
2026-03-12 12:04:16 -0500

    API Tradicional vs API Reactivo

---
+++ projects/webflux-playground/src/main/java/dev/magadiflo/app/sec02/TraditionalWebController.java
@@ -0,0 +1,35 @@
+package dev.magadiflo.app.sec02;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/traditional")
+public class TraditionalWebController {
+
+    private final RestClient restClient = RestClient.builder()
+            .requestFactory(new JdkClientHttpRequestFactory()) // A partir de Spring Boot 3.4.x es recomendable agregar este factory
+            .baseUrl("http://localhost:7070")
+            .build();
+
+    @GetMapping(path = "/products")
+    public ResponseEntity<List<Product>> getProducts() {
+        List<Product> products = this.restClient.get()
+                .uri("/demo01/products")
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {
+                });
+
+        log.info("respuesta recibida: {}", products);
+        return ResponseEntity.ok(products);
+    }
+}
