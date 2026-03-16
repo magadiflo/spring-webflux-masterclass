d20236f8a83e5fc6f07320724a9325991c6d8979
Martín Díaz <magadiflo@gmail.com>
2026-03-16 12:19:06 -0500

    Inicialización de la base de datos en aplicaciones Spring WebFlux con R2DBC

--- projects/webflux-playground/src/main/java/dev/magadiflo/app/WebfluxPlaygroundApplication.java
+++ projects/webflux-playground/src/main/java/dev/magadiflo/app/WebfluxPlaygroundApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @EnableR2dbcRepositories(basePackages = "dev.magadiflo.app.${section}")
-@SpringBootApplication(scanBasePackages = "dev.magadiflo.app.${section}")
+@SpringBootApplication(scanBasePackages = {"dev.magadiflo.app.config", "dev.magadiflo.app.${section}"})
 public class WebfluxPlaygroundApplication {
 
     public static void main(String[] args) {
