948aa8a65c61f95a41d75b7acdbf587cd7d7b25e
Martín Díaz <magadiflo@gmail.com>
2026-03-17 13:08:32 -0500

    Paginación con Pageable en Spring Data R2DBC

--- projects/webflux-playground/src/test/java/dev/magadiflo/app/sec03/Lec02ProductRepositoryTest.java
+++ projects/webflux-playground/src/test/java/dev/magadiflo/app/sec03/Lec02ProductRepositoryTest.java
@@ -4,6 +4,9 @@ import dev.magadiflo.app.sec03.repository.ProductRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import reactor.test.StepVerifier;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,6 +27,18 @@ class Lec02ProductRepositoryTest extends AbstractTest {
                     assertThat(product.getDescription()).isEqualTo("iphone 20");
                 })
                 .expectNextCount(2) // Validamos que hay 3 productos en total (1 validado + 2 más)
+                .verifyComplete();
+    }
+
+    @Test
+    void pageable() {
+        Pageable pageable = PageRequest.of(0, 3, Sort.by("price").ascending());
+        this.productRepository.findBy(pageable)
+                .doOnNext(product -> log.info("{}", product))
+                .as(StepVerifier::create)
+                .assertNext(product -> assertThat(product.getPrice()).isEqualTo(200))
+                .assertNext(product -> assertThat(product.getPrice()).isEqualTo(250))
+                .assertNext(product -> assertThat(product.getPrice()).isEqualTo(300))
                 .verifyComplete();
     }
 }
