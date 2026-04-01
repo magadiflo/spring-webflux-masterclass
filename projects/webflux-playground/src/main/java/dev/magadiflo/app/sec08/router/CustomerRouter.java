4d4ed927ca662a2427d7ac7ae9406da1d280d726
Martín Díaz <magadiflo@gmail.com>
2026-04-01 13:10:01 -0500

    Configuración de rutas funcionales (Functional Endpoints)

--- projects/webflux-playground/src/main/java/dev/magadiflo/app/sec08/router/CustomerRouter.java
+++ projects/webflux-playground/src/main/java/dev/magadiflo/app/sec08/router/CustomerRouter.java
@@ -1,14 +1,26 @@
 package dev.magadiflo.app.sec08.router;
 
+import dev.magadiflo.app.sec08.handler.CustomerHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Configuration
 public class CustomerRouter {
+
+    private static final String CUSTOMERS_URI = "/api/v1/customers";
+    private static final String CUSTOMER_ID_PATH = "/{customerId}";
+
     @Bean
-    public RouterFunction<ServerResponse> customerRoutes() {
-        return null;
+    public RouterFunction<ServerResponse> customerRoutes(CustomerHandler handler) {
+        return RouterFunctions.route()
+                .GET(CUSTOMERS_URI, handler::allCustomers)
+                .GET(CUSTOMERS_URI + CUSTOMER_ID_PATH, handler::getCustomer)
+                .POST(CUSTOMERS_URI, handler::saveCustomer)
+                .PUT(CUSTOMERS_URI + CUSTOMER_ID_PATH, handler::updateCustomer)
+                .DELETE(CUSTOMERS_URI + CUSTOMER_ID_PATH, handler::deleteCustomer)
+                .build();
     }
 }
