f5d11820f8a3d606b9aa4e4188b00a62aeb514fe
Martín Díaz <magadiflo@gmail.com>
2026-04-15 12:54:46 -0500

    Probando subida de productos streaming - Ejemplo 01

---
+++ projects/webflux-playground/src/test/java/dev/magadiflo/app/sec10/ProductsUploadDownloadTest.java
@@ -0,0 +1,27 @@
+package dev.magadiflo.app.sec10;
+
+import dev.magadiflo.app.sec10.dto.ProductRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+@Slf4j
+class ProductsUploadDownloadTest {
+
+    private final ProductClient productClient = new ProductClient();
+
+    @Test
+    void uploadProducts() {
+        Flux<ProductRequest> productRequestFlux = Flux.just(new ProductRequest("iphone", 1000))
+                .delayElements(Duration.ofSeconds(10));
+
+        this.productClient.uploadProducts(productRequestFlux)
+                .doOnNext(uploadResponse -> log.info("Recibido: {}", uploadResponse))
+                .then()
+                .as(StepVerifier::create)
+                .verifyComplete();
+    }
+}
