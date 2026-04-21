5a436d6b7d45f1c342fcc417e61d2e6e0455519e
Martín Díaz <magadiflo@gmail.com>
2026-04-21 13:12:54 -0500

    Exponiendo Streaming API

--- projects/webflux-playground/src/main/java/dev/magadiflo/app/sec11/controller/ProductController.java
+++ projects/webflux-playground/src/main/java/dev/magadiflo/app/sec11/controller/ProductController.java
@@ -2,17 +2,19 @@ package dev.magadiflo.app.sec11.controller;
 
 import dev.magadiflo.app.sec11.dto.ProductRequest;
 import dev.magadiflo.app.sec11.dto.ProductResponse;
-import dev.magadiflo.app.sec11.dto.UploadResponse;
 import dev.magadiflo.app.sec11.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,17 +24,18 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @GetMapping(path = "/download", produces = MediaType.APPLICATION_NDJSON_VALUE)
-    public Mono<ResponseEntity<Flux<ProductResponse>>> downloadProducts() {
-        return Mono.fromSupplier(() -> ResponseEntity.ok(this.productService.findAllProducts()));
+    @GetMapping(path = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Mono<ResponseEntity<Flux<ProductResponse>>> productStream() {
+        return Mono.fromSupplier(() -> ResponseEntity.ok(this.productService.getProductStream()));
     }
 
-    @PostMapping(path = "/upload", consumes = MediaType.APPLICATION_NDJSON_VALUE)
-    public Mono<UploadResponse> uploadProducts(@RequestBody Flux<ProductRequest> productRequestFlux) {
-        log.info("Iniciando el proceso de upload de products");
-
-        return this.productService.saveProducts(productRequestFlux.doOnNext(productRequest -> log.info("ProductRequest: {}", productRequest)))
-                .then(this.productService.countProducts())
-                .map(count -> new UploadResponse(UUID.randomUUID(), count));
+    @PostMapping
+    public Mono<ResponseEntity<ProductResponse>> saveProduct(@RequestBody Mono<ProductRequest> productRequestMono) {
+        return productRequestMono
+                .flatMap(this.productService::saveProduct)
+                .map(productResponse -> ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body(productResponse)
+                );
     }
 }
