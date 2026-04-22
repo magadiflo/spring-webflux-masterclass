27cedf78f55e5ce397b15a2269c1c9d8e00be64e
Martín Díaz <magadiflo@gmail.com>
2026-04-22 12:56:33 -0500

    Caso de uso: Implementación de Filtro de Precios - 3

--- projects/webflux-playground/src/main/java/dev/magadiflo/app/sec11/service/impl/DataSetupService.java
+++ projects/webflux-playground/src/main/java/dev/magadiflo/app/sec11/service/impl/DataSetupService.java
@@ -1,8 +1,7 @@
 package dev.magadiflo.app.sec11.service.impl;
 
+import dev.magadiflo.app.sec11.service.ProductService;
 import dev.magadiflo.app.sec11.dto.ProductRequest;
-import dev.magadiflo.app.sec11.mapper.ProductMapper;
-import dev.magadiflo.app.sec11.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
@@ -17,17 +16,15 @@ import java.util.concurrent.ThreadLocalRandom;
 @Service
 public class DataSetupService implements CommandLineRunner {
 
-    private final ProductRepository productRepository;
-    private final ProductMapper productMapper;
+    private final ProductService productService;
 
     @Override
     public void run(String... args) throws Exception {
         Flux.range(1, 1000)
                 .delayElements(Duration.ofSeconds(1))
                 .map(i -> new ProductRequest("product-" + i, ThreadLocalRandom.current().nextInt(1, 100)))
-                .map(this.productMapper::toProduct)
-                .flatMap(this.productRepository::save)
-                .doOnNext(product -> log.info("product saved: {}", product))
+                .flatMap(this.productService::saveProduct)
+                .doOnNext(productResponse -> log.info("product saved: {}", productResponse))
                 .subscribe();
     }
 }
