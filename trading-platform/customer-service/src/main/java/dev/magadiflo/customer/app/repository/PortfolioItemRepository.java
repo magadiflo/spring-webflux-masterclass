6c9201dd0184ca5981e96f3fcb9c11f6a79ee6ac
Martín Díaz <magadiflo@gmail.com>
2026-04-26 21:03:24 -0500

    Excepciones y clase utilitaria

--- trading-platform/customer-service/src/main/java/dev/magadiflo/customer/app/repository/PortfolioItemRepository.java
+++ trading-platform/customer-service/src/main/java/dev/magadiflo/customer/app/repository/PortfolioItemRepository.java
@@ -1,6 +1,7 @@
 package dev.magadiflo.customer.app.repository;
 
 import dev.magadiflo.customer.app.entity.PortfolioItem;
+import dev.magadiflo.customer.app.enums.Ticker;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -8,5 +9,5 @@ import reactor.core.publisher.Mono;
 public interface PortfolioItemRepository extends ReactiveCrudRepository<PortfolioItem, Long> {
     Flux<PortfolioItem> findByCustomerId(Long customerId);
 
-    Mono<PortfolioItem> findByCustomerIdAndTicker(Long customerId, String ticker);
+    Mono<PortfolioItem> findByCustomerIdAndTicker(Long customerId, Ticker ticker);
 }
